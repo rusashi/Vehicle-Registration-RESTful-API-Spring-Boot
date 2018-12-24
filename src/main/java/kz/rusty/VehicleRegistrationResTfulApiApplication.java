2b860aae0c1fa38813f59469cae56195f0d2d9f2
Rustam Issabekov <rus.issabekov@gmail.com>
2018-12-24 21:34:29 +1100

    initial commit

---
+++ src/main/java/kz/rusty/VehicleRegistrationResTfulApiApplication.java
@@ -0,0 +1,14 @@
+package kz.rusty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegistrationResTfulApiApplication.class, args);
+	}
+
+}
+
