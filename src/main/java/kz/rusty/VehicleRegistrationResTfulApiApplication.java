b671681c56aea1aed3728c74174579c80146d066
Rustam Issabekov <rus.issabekov@gmail.com>
2018-12-24 22:32:01 +1100

    Proof of concept that H2 database is working

--- src/main/java/kz/rusty/VehicleRegistrationResTfulApiApplication.java
+++ src/main/java/kz/rusty/VehicleRegistrationResTfulApiApplication.java
@@ -1,10 +1,27 @@
 package kz.rusty;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VehicleRegistrationResTfulApiApplication {
+	
+	@Autowired
+	private JdbcTemplate template;
+	
+	@RequestMapping("/vehicles")
+	public List<Map<String,Object>> vehicles() {
+		return template.queryForList("select * from vehicle");		
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleRegistrationResTfulApiApplication.class, args);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create table vehicle (
+    id int,
+    make varchar2(50),
+    model varchar2(50),
+    colour varchar2(50),
+    engine_size decimal(10,2)
+);
+
+insert into vehicle values (1, 'Ford', 'Fiesta', 'White', 1.0);
